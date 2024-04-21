5c0b30b2d885244cf1bce7ff61ed30eaf79640d3
ahmad <ahmad@slash.co>
2024-04-21 13:54:14 +0700

    (your-order): add your order page

---
+++ src/app/views/your-order/your-order.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-order',
+  templateUrl: './your-order.component.html',
+  styleUrls: ['./your-order.component.scss']
+})
+export class YourOrderComponent {
+
+}
